3eb14ebee23b23c69f6b856b804310bbb8fb682b
DeanRother <34360462+DeanRother@users.noreply.github.com>
2019-01-03 15:43:39 -0600

    Added post route to insert data into database.

--- thankyou.js
+++ thankyou.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var sql = require('mssql');
+var bodyParser = require('body-parser');
 
 
 var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+//app.use('/static', express.static(__dirname + 'views'));
+
+app.get('/', function(req, res){
+  res.type('text/text');
+  res.send('Thank you email home page.');
+  //res.type('text/html');
+  //res.render('/thankyou');
+});
 
 app.set('port',process.env.PORT||3000);
 
@@ -30,9 +41,41 @@ app.get('/sql', function(req,res){
   });
 });
 
-app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Thank you email home page.');
+app.get('/sql/:id', function(req,res){
+  new sql.ConnectionPool(config).connect().then(pool => {
+    return pool.request().query('select * from ThankYouEmail where ID = ' + req.params.id);
+  }).then(result => {
+    let rows = result.recordset;
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.status(200).json(rows);
+    sql.close();
+  }).catch(err => {
+    res.status(500).send(err);
+    sql.close();
+  });
+});
+
+app.post('/sql', function(req,res){
+  var qry = "insert into dbo.ThankYouEmail(Gift,Giver,EmailAddress,EmailNote) ";
+  qry += "values(";
+  qry += "'" + req.body.Gift + "',";
+  qry += "'" + req.body.Giver + "',";
+  qry += "'" + req.body.EmailAddress + "',";
+  qry += "'" + req.body.EmailNote + "'";
+  qry += ")";
+  console.log(qry);
+  new sql.ConnectionPool(config).connect().then(pool => {
+    return pool.request().query(qry);
+  }).then(result => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //let rows = result.rowsAffected;
+    //res.status(200).json(rows);
+    res.type('text/plain');
+    res.send("Rows affected: " + res.rowsAffected);
+  }).catch(err => {
+    res.status(500).send(err);
+    sql.close();
+  });
 });
 
 app.get('/fme', function(req,res){
